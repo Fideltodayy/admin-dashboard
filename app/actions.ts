c4106291989d0b70d39da6ad69ecb00009abef77
Fideltodayy <fidelotieno11@gmail.com>
2024-05-13 08:03:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/actions.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import { deleteUserById } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+
+export async function deleteUser(userId: number) {
+  // Uncomment this to enable deletion
+  // await deleteUserById(userId);
+  // revalidatePath('/');
+}
